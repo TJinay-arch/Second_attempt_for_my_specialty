cd948780bfce55681749a5935471e264f23b4bae
Ilya Payusov <ilya-payusov@mail.ru>
2025-09-03 05:12:56 +0300

    complete 9.1 homework

--- src/masks.py
+++ src/masks.py
@@ -1,19 +1,12 @@
 def get_mask_card_number(input_card: str) -> str:
-    """Функция принимает на вход номер и возвращает её маску
-    :rtype: str
-    """
+    """Функция принимает на вход номер и возвращает её маску"""
 
-    # Преобразуем ввод в список символов карты
     card_number = list(input_card)
 
-    # Перебираем индексы
     for i in range(len(card_number)):
-        # Если позиция индекса находится между шестым и двенадцатым символом включительно,
-        # замещаем её звездочкой (*).
         if 6 <= i <= 11:
             card_number[i] = "*"
 
-    # Формируем итоговую маску с пробелами каждые четыре цифры
     masked_string = "".join(card_number[:4]) + " "
     masked_string += "".join(card_number[4:8]) + " "
     masked_string += "".join(card_number[8:12]) + " "
--- src/widget.py
+++ src/widget.py
@@ -3,6 +3,7 @@ from src.masks import get_mask_card_number, get_mask_account
 
 def mask_account_card(input_number: str) -> str:
     """Функция обрабатывает информацию как о счетах, так и номерах карт"""
+
     parts = input_number.split()
     if parts[0] == "Счет":
         name_part: str = "".join(parts[:-1])
@@ -13,10 +14,15 @@ def mask_account_card(input_number: str) -> str:
         card_part: str = get_mask_card_number(parts[-1])
         return f"{name_of_a_card_part} {card_part}"
 
+
 def get_date(input_string: str) -> str:
-    parts_of_data = input_string.split('-')
+    """функция принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ"("11.03.2024")"""
+
+    parts_of_data = input_string.split("-")
     year_from_string = parts_of_data[-1]
-    output_date = year_from_string[0:2] + '.'
-    output_date += ''.join(parts_of_data[1]) + '.'
-    output_date += ''.join(parts_of_data[0])
+    output_date = year_from_string[0:2] + "."
+    output_date += "".join(parts_of_data[1]) + "."
+    output_date += "".join(parts_of_data[0])
+
     return output_date
