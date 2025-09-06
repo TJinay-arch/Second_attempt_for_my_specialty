4fa5e1ff6daf506877698c41fa985437379ad7d3
Ilya Payusov <ilya-payusov@mail.ru>
2025-09-06 15:57:09 +0300

    homework_10.2

---
+++ src/processing.py
@@ -0,0 +1,17 @@
+def filter_by_state(data: list[dict[str, str]], state = 'EXECUTED') -> list[dict[str, str]]:
+    ''' Функция фильтрует список словарей по ключу state'''
+
+    filtered_data = []
+    i = 0
+    for dictionary in data:
+        for key, value in data[i].items():
+            if key == 'state' and value == state:
+                filtered_data.append(data[i])
+        i += 1
+    return filtered_data
+
+def sort_by_date(date_list: list[dict[str, str]], param: bool = False) -> list[dict[str, str]]:
+    ''' Функция сортирует список словарей по ключу date '''
+
+    sorted_list = sorted(date_list, key=lambda x: x['date'], reverse=param)
+    return sorted_list
